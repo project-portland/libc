4c803c963d631e31b5d1fac1430405798e7da2a9
benjidial <Benji3.141@gmail.com>
2018-10-05 11:33:01 -0400

    part of limits

---
+++ src/headers/limits.h
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * * *
+ * Portland libc limits.h    *
+ * Copyright 2018 Benji Dial *
+ * Under GNU GPL v3.0        *
+ * * * * * * * * * * * * * * */
+
+#ifndef __LIBC_LIMITS
+#define __LIBC_LIMITS
+
+#define CHAR_BIT 8
+#define SCHAR_MIN -128
+#define SCHAR_MAX 127
+#define UCHAR_MAX 255
+#define CHAR_MIN 0
+#define CHAR_MAX 255
+#define MB_LEN_MAX 2
+#define SHRT_MIN -32768
+#define SHRT_MAX 32767
+#define USHSRT_MAX 65535
+#define INT_MIN -2147483648
+#define INT_MAX 2147483647
+#define UNIT_MAX 4294967295
+#define LONG_MIN /*TODO*/
+#define LONG_MAX /*TODO*/
+#define ULONG_MAX /*TODO*/
+#define LLONG_MIN /*TODO*/
+#define LLONG_MAX /*TODO*/
+#define ULLONG_MAX /*TODO*/
+
+#endif
