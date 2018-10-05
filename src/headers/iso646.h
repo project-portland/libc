07cea1a890bb027da918dbbbaa33e6b6381b6887
benjidial <Benji3.141@gmail.com>
2018-10-05 11:32:49 -0400

    iso645

---
+++ src/headers/iso646.h
@@ -0,0 +1,22 @@
+/* * * * * * * * * * * * * * *
+ * Portland libc iso646.h    *
+ * Copyright 2018 Benji Dial *
+ * Under GNU GPL v3.0        *
+ * * * * * * * * * * * * * * */
+
+#ifndef __LIBC_ISO646
+#define __LIBC_ISO646
+
+#define and &&
+#define and_eq &=
+#define bitand &
+#define bitor |
+#define compl ~
+#define not !
+#define not_eq !=
+#define or ||
+#define or_eq |=
+#define xor ^
+#define xor_eq ^=
+
+#endif
