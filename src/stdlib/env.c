b4ba8125c39ed156f6596c8f79589549263ac50f
Benji Dial <benji3.141@gmail.com>
2018-10-05 13:25:46 -0400

    `exit` in `stdlib` now supports registering functions during functions

--- src/stdlib/env.c
+++ src/stdlib/env.c
@@ -9,38 +9,48 @@
 
 static struct func_block {
   void (*funcs[32])(void);
-  size_t n_funcs = 0;
+  size_t n_funcs;
   struct func_block *prev;
-} *last_func_block = malloc(sizeof(struct func_block));
+} *last_func_block = NULL;
 
 void abort(void) {
   /*TODO*/
 }
 
 int atexit(void (*func)(void)) {
-  if (last_func_block->n_func == 32) {
-    struct func_block *tmp = last;
-    if (last = malloc(sizeof(struct func_block))) {
-      last->prev = tmp;
-      last->n_funcs = 1;
-      last->funcs[0] = func;
+  if (last_func_block == NULL) {
+    if (last_func_block = malloc(sizeof(struct func_block))) {
+      last_funck_block->prev = NULL;
+      last_funck_block->n_funcs = 1;
+      last_funck_block->func[0] = func;
       return 0;
     }
-    last = tmp;
+    return 1;
+  } if (last_func_block->n_func == 32) {
+    struct func_block *tmp = last_func_block;
+    if (last_funck_block = malloc(sizeof(struct func_block))) {
+      last_funck_block->prev = tmp;
+      last_funck_block->n_funcs = 1;
+      last_funck_block->funcs[0] = func;
+      return 0;
+    }
+    last_funck_block = tmp;
     return 1;
   }
-  last->funcs[last->n_funcs++] = func;
+  last_funck_block->funcs[last_funck_block->n_funcs++] = func;
   return 0;
 }
 
-/*FIXME: cannot handle registered functions registering more functions*/
 void exit(int status) {
-  struct func_block *tmp = last;
-  do {
-    size_t i = tmp->n_funcs;
-    while (i--)
-      tmp->funcs[i]();
-  } while (tmp = tmp->prev);
+  while (last_func_block) {
+    struct func_block *tmp = last_func_block;
+    last_func_block = NULL;
+    do {
+      size_t i = tmp->n_funcs;
+      while (i--)
+        tmp->funcs[i]();
+    } while (tmp = tmp->prev);
+  }
   _Exit(status);
 }
 
