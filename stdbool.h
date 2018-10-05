db07f2c08f675b6f1eab075308fcfd2d6afa8a73
benjidial <Benji3.141@gmail.com>
2018-10-04 21:38:24 -0400

    stdbool.h

---
+++ stdbool.h
@@ -0,0 +1,10 @@
+/* * * * * * * * * * * * * * *
+ * Portland libc stdbool.h   *
+ * Copyright 2018 Benji Dial *
+ * Under GNU GPL v3.0        *
+ * * * * * * * * * * * * * * */
+
+#define bool _Bool
+#define true 1
+#define false 0
+#define __bool_true_false_are_defined 1
